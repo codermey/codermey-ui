0fd2c9f37a4556fbe31991fd38bf4d5637a8e885
codermey <18672267024@163.com>
2025-01-18 16:27:08 +0800

    feat(styles): 公共样式配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Logo from "@/assets/logo.png";
 import HelloWorld from "@/components/HelloWorld/HelloWorld";
-
-import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
+    <main className="semi-always-light">
+      {/*<img alt="React logo" width="400px" src={Logo} />*/}
       <HelloWorld msg="Hello React + TypeScript + Vite" />
     </main>
   );
