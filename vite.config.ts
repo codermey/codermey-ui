c49209e313f0f4b106d423ec5a5d42d5be187695
马恩源 <maenyuan@tenqent.com>
2025-01-18 15:41:10 +0800

    feat(deps): 使用vite打包library配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,49 @@
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [legacy(), react()],
-  resolve: {
-    alias: {
-      // for TypeScript path alias import like : @/x/y/z
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        secure: false,
-        rewrite: path => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: isDev ? [legacy(), react()] : [],
+    resolve: {
+      alias: {
+        // for TypeScript path alias import like : @/x/y/z
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          secure: false,
+          rewrite: path => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/components/index.tsx"),
+        name: "CodermeyUI",
+        fileName: format => `codermey-ui.${format}.js`,
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ["react", "react-dom"],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      },
+    },
+  };
 });
